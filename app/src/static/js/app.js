3bb388059b8abcdca65339961fabd546ec40e976
Jack O'Shea <jackoshea4038@gmail.com>
2022-09-29 21:05:15 +0000

    docs: :art: Added comment to changed line
    Added code which displays when there is no current items

--- app/src/static/js/app.js
+++ app/src/static/js/app.js
@@ -53,6 +53,7 @@ function TodoListCard() {
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
+                //Modified Code with correct line.
                  <p className="text-center">You have no todo items yet! Add one above!</p>
             )}
             {items.map(item => (
